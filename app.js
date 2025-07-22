1505f1dafd3385a551355b8d6cf4f5c0c2757d31
Bigben4 <130978886+Bigben4@users.noreply.github.com>
2025-07-22 05:03:40 -0700

    added  a todo list

--- app.js
+++ app.js
@@ -18,3 +18,34 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+
+
+// todo fuctionslities 
+
+
+ const todoList = document.getElementById('todo-list');
+        const addBtn = document.getElementById('add-todo-btn');
+        const viewAllBtn = document.getElementById('view-all-todo-btn');
+        let todos = [];
+
+        addBtn.onclick = function() {
+            const todoText = prompt('Enter a new to do:');
+            if (todoText) {
+                todos.push(todoText);
+                renderTodos();
+            }
+        };
+
+        viewAllBtn.onclick = function() {
+            renderTodos();
+        };
+
+        function renderTodos() {
+            todoList.innerHTML = '';
+            todos.forEach((todo, idx) => {
+                const li = document.createElement('li');
+                li.textContent = todo;
+                todoList.appendChild(li);
+            });
+        }
